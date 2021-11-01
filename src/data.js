eab4e40e937d71fcd1dca8dd7af9dd711adaa0ba
asapconet <aaronrickymeek@gmail.com>
2021-11-01 17:48:19 +0100

    completed functionality milstone

--- src/data.js
+++ src/data.js
@@ -1,37 +1,43 @@
 const myDates = [
   {
     id: "a1",
-    name: "albert drew",
+    name: "top shit",
     age: "34",
     img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
   },
 
   {
     id: "a2",
-    name: "albert drew",
+    name: "tuedon sticky",
     age: "34",
     img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
   },
 
   {
     id: "a3",
-    name: "albert drew",
+    name: "shehi musah",
     age: "34",
     img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
   },
 
   {
     id: "a4",
-    name: "albert drew",
+    name: "lord sarcastic",
     age: "34",
     img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
   },
 
   {
     id: "a5",
-    name: "albert drew",
+    name: "erons owo",
+    age: "34",
+    img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
+  },
+  {
+    id: "a5",
+    name: "asap a1",
     age: "34",
     img: "https://yt3.ggpht.com/ytc/AKedOLS9uHBwlU7ia1YWnOfHNrThpVxy2oWAZv-HK99eHQ=s900-c-k-c0x00ffffff-no-rj",
   },
 ];
-export default myDates
+export default myDates;
