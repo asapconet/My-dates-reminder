d8016dd035eac17d79197cd7cefe201155533355
asapconet <aaronrickymeek@gmail.com>
2021-11-01 16:05:41 +0100

    created all data for consumption

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import myData from "./data";
 
 function App() {
+  const [data, setData] = useState(myData);
+
   return (
-   <main>
-     <div className='main-card'> 
-     <button className='btn'>Clear all</button>
-     </div>
-   </main>
+    <main>
+      <div className="main-card">
+        <h3>You've signed up for{} dates this week</h3>
+        {data.map((e) => {
+          const { id, name, img, age } = e;
+          return (
+            <article key={id}>
+              <img src={img} alt={name}/>
+              <div>
+                {name}
+                <p>{age}</p>
+              </div>
+            </article>
+          );
+        })}
+        <button className="btn">Clear all</button>
+      </div>
+    </main>
   );
 }
 
